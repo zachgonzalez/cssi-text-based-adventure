7e1ccd9c1cfaba66534932dd6d89d86efa0eeba7
CSSI 2015 <CSSI@CSSIs-MacBook-Air.local>
2015-08-03 17:00:12 -0500

    Adding css and js

--- cssi-text-based-adventure/main.py
+++ cssi-text-based-adventure/main.py
@@ -33,11 +33,15 @@ class MainHandler(webapp2.RequestHandler):
 
 class GameHandler(webapp2.RequestHandler):
     def get(self):
+        start_text = "Your identical twin has set you up. He told you to meet him downtown at the Board of Trade building, and when you arrive you realize that he has robbed the commissioners and is using you as a doppelganger. You don't realize this until you see the news in a store window nearby announcing the breaking news. Miraculously, you realize that you haven't been caught yet because the police think that the twin is still in the building and that he is wearing all black, but you are about a block away and wearing pastel colors. You have to get away from the scene quickly or you will have to pay a pretty bad price. Without a second thought, you decide you should run somewhere. To the north is Madison Street, to the west is a deserted alley, to the east is Wacker Drive, and to the south is Monroe Street."
+        beginning = {"story_text": start_text}
         template = JINJA_ENVIRONMENT.get_template('templates/index.html')
-        self.response.out.write(template.render())
+        self.response.out.write(template.render(beginning))
+
     def post(self):
         user_direction = self.request.get('user_direction')
-        user_direction_template_vars = {"direction": user_direction}
+        story1 = "This is what happens when they go north..."
+        user_direction_template_vars = {"direction": user_direction, "story_text": story1}
         if user_direction.lower() == 'north' or user_direction.lower() == 'south' or user_direction.lower() == 'east' or user_direction.lower() == 'west':
 
             template = JINJA_ENVIRONMENT.get_template('templates/index.html')
